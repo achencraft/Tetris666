161f511a876d0207a18b371059da7790bffed558
Lucas LETT <lucas.3@laposte.net>
2021-02-19 18:04:11 +0100

    push

--- Piece.cpp
+++ Piece.cpp
@@ -1,3 +1,11 @@
 #include "Piece.h"
 
 Piece::Piece() {}
+
+Piece::changerOrientation(int nouvelle_orientation) {
+    this.orientation = nouvelle_orientation;
+}
+
+Piece::poser() {
+    this.estPose = true;
+}
